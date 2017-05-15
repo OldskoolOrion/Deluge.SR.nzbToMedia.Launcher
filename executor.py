e07eeddfd1456f1025a3d905d9101597e6f29ee5
Oldskool Orion <OldskoolOrion@users.noreply.github.com>
2017-05-16 01:19:10 +0200

    Tweaks to license text & personalia
    Working towards standard header when releasing under MIT license. Most tweaks in header.
    Done for now. New American Gods episode to watch !

--- executor.py
+++ executor.py
@@ -1,31 +1,43 @@
 #!/usr/bin/env python
+
 #-------------------------------------------------------------------------------
-# Copyright (C) 2017 Harold Coenen
+# MIT License
+# 
+# Copyright (c) 2017  -  Harold Coenen ( a.k.a. OldskoolOrion )
 #
-# Permission is hereby granted, without written agreement and without
-# license or royalty fees, to use, copy, modify, and distribute this
-# software and its documentation for any purpose, provided that the
-# above copyright notice and the following two paragraphs appear in
-# all copies of this software.
+# E-mail :
+#     OldskoolOrion@gmail.com
 #
-# IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE TO ANY PARTY FOR
-# DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
-# ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN
-# IF THE COPYRIGHT HOLDER HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
-# DAMAGE.
-#
-# THE COPYRIGHT HOLDER SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING,
-# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-# FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
-# ON AN "AS IS" BASIS, AND THE COPYRIGHT HOLDER HAS NO OBLIGATION TO
-# PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+# First shared at GitHub (2017.05.16) - v0.1.0 :
+#     https://github.com/OldskoolOrion/Deluge.SR.nzbToMedia.Launcher.git
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #-------------------------------------------------------------------------------
+
 #-------------------------------------------------------------------------------
-# Name:         executor.py
-# Version:      0.1.0
-# Created:      2017.05.15
-# Copyright:    OldskoolOrion (Harold Coenen)
-# License:      MIT
+# Name      : executor.py
+# Version   : 0.1.2
+# Created   : 2017.05.16
+# License   : MIT
+# Copyright : (c) 2017  -  Harold Coenen ( a.k.a. OldskoolOrion )
+# E-mail    : OldskoolOrion@gmail.com
+# Location  : https://github.com/OldskoolOrion/Deluge.SR.nzbToMedia.Launcher.git
 #-------------------------------------------------------------------------------
 
 # import modules
