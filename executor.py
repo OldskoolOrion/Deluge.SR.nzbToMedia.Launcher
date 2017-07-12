e1b9045b27d1cbd9e04fc9f7a0652d2b436baa47
OldskoolOrion <OldskoolOrion@gmail.com>
2017-07-13 01:51:00 +0200

    rethought proces & described steps

--- executor.py
+++ executor.py
@@ -1,102 +1,166 @@
 #!/usr/bin/env python
 
-#-------------------------------------------------------------------------------
-# MIT License
-# 
-# Copyright (c) 2017  -  Harold Coenen ( a.k.a. OldskoolOrion )
-#
-# E-mail :
-#     OldskoolOrion@gmail.com
-#
-# First shared at GitHub (2017.05.16) - v0.1.0 :
-#     https://github.com/OldskoolOrion/Deluge.SR.nzbToMedia.Launcher.git
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#-------------------------------------------------------------------------------
+"""
+----------------------------------------------------------------------------------------------------
+  MIT License                                                                   Copyright (c) 2017
+----------------------------------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------------
-# Name      : executor.py
-# Version   : 0.1.5
-# Last mod. : 2017.05.27
-# Created   : 2017.05.16
-# License   : MIT
-# Copyright : (c) 2017  -  Harold Coenen ( a.k.a. OldskoolOrion )
-# E-mail    : OldskoolOrion@gmail.com
-# Location  : https://github.com/OldskoolOrion/Deluge.SR.nzbToMedia.Launcher.git
-#-------------------------------------------------------------------------------
+- Author :
+    Harold Coenen (OldskoolOrion)
+- E-mail :
+    OldskoolOrion@gmail.com
+- First shared at GitHub (2017.05.16) - v0.1.0 :
+    https://github.com/OldskoolOrion/Deluge.SR.nzbToMedia.Launcher.git
 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-##  TESTDATA -> invoke on cli :
-##  D:\_Development_\PortableGit\home\OldskoolOrion\Deluge.SR.nzbToMedia.Launcher\executor.py "d2d6a72b60cdb2cc5e80d3277d89d5df18c3ecbc" "Logan.2017.1080p.WEB-DL.DD5.1.H264-FGT" "D:\Feed.Me.Bytes\u.movie"
+    1) The above copyright notice and this permission notice shall be included in all copies or
+       substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+---
+
+Scriptname      : executor.py
+Version         : 0.1.8
+Last mod.       : 2017.07.12
+Created         : 2017.05.16
+License         : MIT
+Copyright       : (c) 2017, Harold Coenen (OldskoolOrion)
+E-mail          : OldskoolOrion@gmail.com
+Scriptlocation  : https://github.com/OldskoolOrion/Deluge.SR.nzbToMedia.Launcher.git
+"""
+
+####################################################################################################
+############
 ##
-##  Commandline command      : pyw D:\Feed.Me.Bytes\d.downloading\config\TorrentToMedia.py "193593c6678281cc3fd2b4e23232747c4855820f" "Last.Week.Tonight.with.John.Oliver.S04E12.720p.WEBRip.AAC2.0.H264-doosh[rarbg]" "D:\Feed.Me.Bytes\u.series"
-##  Directory to clean       : D:\Feed.Me.Bytes\u.series\Last.Week.Tonight.with.John.Oliver.S04E12.720p.WEBRip.AAC2.0.H264-doosh[rarbg]
-##  Filemasks to delete      : links nfo exe chm nzb par sfv srr pics txt numbered OSX RARBG.* Goedkoop*.rar
-##  Former simplyfied delete : FOR %%g IN ( *.nzb *.par2 *.par *.sfv *.srr *.jpg *.5 *.4 *.3 *.2 *.1 *.gif *.png *.txt *.nfo *.url Q-for-You.* *.exe *.chm *._Contents *.plist *.wflow *.workflow RARBG.* Goedkoop*.rar ) DO ( DEL "%%g" )
-##  ==============================================================================================================================================================================================================================================
-##  NOTE : sometimes the directory is passed as argument in the following format :  \\?\D:\etc
-##       : when this is the case, transform it to standard notation              :  D:\etc
-##  CODE : SET resultdir=%~3\%~2
-##       : SET resultdir=%resultdir:\\?\=%
-
+##  STEPS TO TAKE :
+##  Run As (Dictated by Deluge) :
+##      executor.py "d2d6a72b60cdb2cc5e80d3277d89d5df18c3ecbc" "Name.S01E01.720p.WEBRip.H264-GRP" \
+##                  "D:\TorrentDL\u.series"
+##  Check if PARAM 2 is file or directory :
+##      D:\TorrentDL\u.series\Name.S01E01.720p.WEBRip.H264-GRP(.mkv) ??
+##          -> note : Extremely basic but therefor easy to overlook check!
+##             Initiating torrent downloads by scripts using magnet-links, sometimes prevents you
+##             from noticing the movie/serie-episode download is not in a folder of it's own.
+##             Usually this is standard practice. Is there just one single file not in a folder ?
+##             This of course has consequences when trying to tidy/clean the release from as much
+##             unnecessary extra files and even worse (malware/virus).
+##             Imagine not checking : chdir to a file instead of folder - script will not halt, but
+##             no directory change was made. Still in script directory the script starts cleaning.
+##             You could lose all your scripts / other files, or at least spending time restoring
+##             a backup. Prevent extra work.- automation is the goal : not extra work!
+##  Clean Folder after handling possible exception, based on experience :
+##      D:\TorrentDL\u.series\Name.S01E01.720p.WEBRip.H264-GRP
+##          Filemasks which are being deleted for movies / serie-episodes :
+##              - *.links    - *.nfo    - *.bat
+##              - *.exe      - *.chm    - *.cmd
+##              - *.nzb      - *.par    - *.vbs
+##              - *.par2     - *.sfv    - *.ps1
+##              - *.srr      - *.jpg    - *.js
+##              - *.png      - *.bmp    - *.py
+##              - *.ico      - *.txt    - *.[docx|xlsx|pptx]
+##              - *.[1..99]  - *.OSX (and OSX typical files)
+##              - RARBG.*    - *.(thumbs db)
+##              - *.pdf      - Goedkoop*.rar
+##              (and so on.. edit list for personal preference / need)
+##          Comparable to what is often done with a batchfile function under Windows (example) :
+##          >> FOR %%g IN ( *.nzb *.par2 *.par *.sfv *.srr *.jpg *.gif *.png *.txt *.nfo *.url \
+##          >>              Q-for-You.* *.exe *.chm *._Contents *.plist *.wflow *.workflow RARBG.* \
+##          >>              Goedkoop*.rar ) DO ( DEL "%%g" )
+##  Check videocontainer's first audiostream :
+##      If not produced as E-AC3 stream, then continue to the next step.
+##      If it IS E-AC3 instead of the standard A-AC3 codec, then perform in the background :
+##          - rename container to *.eac3.mkv
+##          - using FFMpeg leave videostream & subtitlestreams untouched
+##          - just reencode audiostream from E-AC3 into A-AC3 and recombine this stream, with the
+##            untouched video & subtitles. This saves A LOT time compared to transcode or full
+##            reencode (as nzbToMedia (see next step) performs.
+##          - when starting the FFMpeg execution, also start a monitor on FFMpeg's ProcessID :
+##            on end of ProcessID delete *.eac3.mkv. This ensures removal of correct input, when
+##            multiple postprocessing threads run ( multiprocessing + nonblocking code ).
+##          - After deleting the file continue to the next step
+##  Continue postprocessing using nzbToMedia's scripts :
+##      Calling nzbToMedia gives additional perk of FDH (failed download handling), when being
+##      coupled to SickRage as main serie-episodes management centre. The proliferation of fakes,
+##      incompletes, corrupt files, codec scams, propagation issues, upload glitches and
+##      DMCA takedowns can break the automation process and requires user intervention.
+##      Not what we want.. we want full automation, with no waiting time!
+##      >> ISSUE : pyw TorrentToMedia.py "193593c6678281cc3fd2b4e23232747c4855820f" \
+##                                       "Name.S01E01.720p.WEBRip.H264-GRP" "D:\TorrentDL\u.series"
+##      End-of-Script!
+##  IMPORTANT: path-notation under Windows sometimes changes :
+##                 * usually pathnotation under Windows   :  "D:\etc"
+##                 * occasionally pathnotation found like :  "\\?\D:\etc"
+##             Make sure both forms are dealt with correctly! Easy string replace would do the
+##             trick (from Windows batch script as example) :
+##                 ' SET resultdir=%~3\%~2 ' and then ' SET resultdir=%resultdir:\\?\=% '
+##
+############
+####################################################################################################
 
 # import modules
-import sys, os
-from datetime import date
+import sys
+import os
+import time
 
 # constants
-executeScript = "TorrentToMedia.py"
-executionPath = "D:\\Feed.Me.Bytes\\d.downloading\\config\\"
+EXECSCRIPT = "TorrentToMedia.py"
+EXECPATH = "D:\\Feed.Me.Bytes\\d.downloading\\config\\"
 
-preProcessed  = 0
-exitCode      = 4 - len(sys.argv)
+if __name__ == "__main__":
 
-if not exitCode == 0:
-    print("** ERROR - Expecting 3 arguments. Check below for an example how to call this script:")
-    print("\n            cli>> path.to/executor.py \"< torrent-id >\"  \"< release-name >\"  \"< destination-path >\" ")
-    print("\n\n** NOTE  : <releaseName> can be a either a video-file, or the directory-name containing the content to process.")
-    print("\nErrorcode: "+str(exitCode))
-    exit(exitCode)
-else:
-    argumentList    = str(sys.argv)
-    torrentID       = sys.argv[1]
-    releaseName     = sys.argv[2]
-    destinationPath = sys.argv[3]
-    combinedPath    = os.path.join(destinationPath, releaseName)
-    
-    if os.path.isdir(combinedPath):
-        print("\nEntering directory : "+str(combinedPath))
-        #change into directory and clean unwanted files
-        preProcessed = 1
+    T1 = time.clock()
+    PREPROCESSED = 0
+    EXITCODE = 4 - len(sys.argv)
+
+    if not EXITCODE == 0:
+        print("** ERROR - 3 arguments expected. See below how to call this script:")
+        print("\n            >> path.to/executor.py \"< torrent-id >\"  \"< release-name >\""\
+              " \"< destination-path >\" ")
+        print("\n\n** NOTE  : <releaseName> can be a either a video-file, or the directory-name"\
+              " containing the content to process.")
+        print("\nErrorcode: "+str(EXITCODE))
+        exit(EXITCODE)
     else:
-        if path.os.isfile(combinedPath):
-            print("\nRelease passed was not a directory but an existing single video-file! : "+str(combinedPath))
-            # create directory & move file
-            preProcessed = 1
-        #else:
-            exitCode = 0xFF
+        ARGLIST = str(sys.argv)
+        TORRENTID = sys.argv[1]
+        RELEASE = sys.argv[2]
+        DESTPATH = sys.argv[3]
+        COMBINEDPATH = os.path.join(DESTPATH, RELEASE)
 
-if not exitCode == 0
-    print("** ERROR detected")
-    print("\nErrorcode: "+str(exitCode))
-else:
-    print("No Errors message + steps taken (preProcessed) (maybe executed script in case of series)")
-    print("\nExiting with code : "+str(exitCode))
+        if os.path.isdir(COMBINEDPATH):
+            print("\nEntering directory : "+str(COMBINEDPATH))
+            #change into directory and clean unwanted files
+            PREPROCESSED = 1
+        else:
+            if os.path.isfile(COMBINEDPATH):
+                print("\nRelease passed was not a directory but an existing single video-file: "
+                      +str(COMBINEDPATH))
+                # create directory & move file
+                PREPROCESSED = 1
+            else:
+                EXITCODE = 0xFF
 
-exit(exitCode)
+    if EXITCODE != 0:
+        print("\n> ERROR detected")
+        print("Errorcode          : "+str(EXITCODE))
+    else:
+        print("No Error : maybe do aditionall stuff before passing it up the chain")
+        print("\nExiting with code : "+str(EXITCODE))
+
+    T2 = time.clock()
+    print("Starttime          : "+str(T1))
+    print("Endtime            : "+str(T2))
+    print("Total runtime (ms) : "+str((T2-T1)*1000))
+    print("Exiting script...")
+
+    exit(EXITCODE)
